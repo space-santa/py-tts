307d2e213a1ae2751f24199679131fa3c2f7e9fc
claus <claus.z@protonmail.com>
2019-03-17 08:46:07 +1100

    Add create_device function to post.py

--- src/output/post.py
+++ src/output/post.py
@@ -2,8 +2,20 @@ import json
 from datetime import datetime, timezone
 
 import requests
+from requests.auth import HTTPBasicAuth
 
 import settings
+
+
+def _post_request(data, endpoint):
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
+    reply = requests.post(
+        f"{settings.API_URL}/{endpoint}/",
+        data=json.dumps(data),
+        headers=headers,
+        auth=HTTPBasicAuth(settings.NAME, settings.PASSWORD),
+    )
+    print(reply.text)
 
 
 def write(temperature):
@@ -12,6 +24,9 @@ def write(temperature):
         "device": settings.NAME,
         "timestamp": str(datetime.now(timezone.utc).astimezone()),
     }
-    headers = {"Content-type": "application/json", "Accept": "text/plain"}
-    reply = requests.post(settings.API_URL, data=json.dumps(data), headers=headers)
-    print(reply.text)
+    _post_request(data, "temperature")
+
+
+def create_device():
+    data = {"name": settings.NAME, "location": settings.LOCATION}
+    _post_request(data, "device")
